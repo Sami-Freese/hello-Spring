a2d83fd83708c2ce78a96a368fbddc841163e5a3
Sami Freese <sami.freese@gmail.com>
2022-05-01 15:02:34 -0500

    project start

---
+++ src/test/java/org/launchcode/helloSpring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.helloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
